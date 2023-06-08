e9388f5839f065698b15a5beccf8eff99980027a
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2023-06-08 16:55:16 +0100

    paru.conf modifications

--- arch_install.sh
+++ arch_install.sh
@@ -89,8 +89,8 @@ cd paru
 makepkg -si
 cd ..
 rm -rf paru
-sed -i "s/^#[bin] = 5$/[bin]/" /etc/paru.conf
-sed -i "s/^#Sudo = doas = 5$/Sudo =doas/" /etc/paru.conf
+sed -i "s/^#[bin]/[bin]/" /etc/paru.conf
+sed -i "s/^#Sudo/Sudo/" /etc/paru.conf
 
 systemctl enable NetworkManager
 systemctl enable gdm
