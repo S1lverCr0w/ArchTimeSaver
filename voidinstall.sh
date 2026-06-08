f70d31a34c877ec0f909736c76785f6db2a92c47
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-06-08 23:06:52 +0100

    void install script 02

--- voidinstall.sh
+++ voidinstall.sh
@@ -38,3 +38,40 @@ mount /dev/nvme1n1p7 /mnt/home/
 xgenfstab -U /mnt > /mnt/etc/fstab
 xchroot /mnt /bin/bash
 
+ln -sf /usr/share/zoneinfo/Europe/Dublin /etc/localtime
+hwclock --systohc
+echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
+xbps-reconfigure -f glibc-locales
+
+echo "KEYMAP=uk" >> /etc/rc.conf
+
+hnm="v_ermao"
+echo "$hnm" > /etc/hostname
+echo "127.0.0.1		localhost" >>/etc/hosts
+echo "::1		        localhost" >>/etc/hosts
+echo "127.0.1.1		$hnm.localdomain $hnm" >>/etc/hosts
+
+# # adding user
+echo "set root password and make sure numlock is enabled"
+passwd
+echo "set login username. no capitals allowed"
+read username
+useradd -m $usernm
+echo "set password for $usernm"
+passwd $usernm
+usermod -aG wheel,audio,video,storage $usernm
+
+
+# # install
+xbps-install -S vim
+xbps-install -S opendoas
+
+xbps-install -S grub-x86_64-efi
+echo "permit persist $username as root" >/etc/doas.conf
+sed -i "s/^# %wheel ALL=(ALL:ALL) ALL$/%wheel ALL=(ALL:ALL) ALL/" /etc/sudoers.dist
+# mkdir /boot/EFI
+# mount /dev/nvme0n1p3 /boot/EFI
+
+#for windows dualboot only
+mkdir /boot/WINDOWS
+mount /dev/nvme0n1p1 /boot/WINDOWS
